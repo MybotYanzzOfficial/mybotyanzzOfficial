7a8ea909ae4d1d799195e4fa1f3570ba2bbf2a3a
MybotYanzzOfficial <77045960+MybotYanzzOfficial@users.noreply.github.com>
2021-01-07 19:43:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
+const BotName = 'Yanzz Official Qu☄'; 
+const instagram = 'https://instagram.com/zxuan54'; 
 const telegram = 'https://t.me/reuzzz'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
